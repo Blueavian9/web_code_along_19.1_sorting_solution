7396842eaa5fa544b3d94411fe3f8c8cd9584566
Cesar Aguilar <Blueavian9@gmail.com>
2023-08-30 22:57:07 -0700

    Initial commit

--- merge_sort.js
+++ merge_sort.js
@@ -26,14 +26,81 @@
 //   const unsortedList = [1, 97, 36, -4, 0, 124, 3000];
 // }
 
+/*
+0.
+a.
 function mergeSort(inputList) {
   const { length } = inputList;
   if (length <= 1) return inputList;
 }
+b.
+const middleIndex = Math.floor(length / 2);
+let = inputList.slice(0, middleIndex);
+let = inputList.slice(middleIndex, length);
+
+return merge(mergeSort(left), mergeSort(right));
+c.
+function merge(left, right) {
+  let arr = [];
+  
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+  
+  return [...arr, ...left, ...right];
+  
+  // Test cases: 
+  const unsortedList = [9, 27, 36, -4, 0, 124, 3000];
+}
+
+
+
+
+
+1
+function mergeSort(inputList) {}
+const { length } = inputList;
+if (length <= 1) return inputList;
 
 const middleIndex = Math.floor(length / 2);
 let = inputList.slice(0, middleIndex);
 let = inputList.slice(middleIndex, length);
+
+
+return merge(mergeSort(left), mergeSort(right));
+
+function merge(left, right) {
+  let arr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right.[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+~
+  return [...arr, ...left, ...right];
+}
+
+// Test cases:
+const unsortedList = [9, 27, 36, -4, 0, 124, 3000];
+}
+
+2
+function mergeSort(inputList) {}
+const { length } = inputList;
+if (length <= 1) return inputList;
+
+
+const middleIndex = Math.floor(length / 2);
+let = inputList.slice(0, middleIndex);
+let = inputList.slice(middleIndex, length);
+
 
 return merge(mergeSort(left), mergeSort(right));
 
@@ -45,10 +112,278 @@ function merge(left, right) {
       arr.push(left.shift());
     } else {
       arr.push(right.shift());
-    }
   }
-
-  return [...arr, ...left, ...right];
-
-  const unsortedList = [9, 27, 36, -4, 0, 124, 3000];
 }
+
+return [...arr, ...left, ...right];
+}
+
+// Test cases:
+const unsortedList = [9, 27, 36, -4, 0, 124, 3000];
+}
+
+
+
+
+
+3
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+4
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+5
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+6
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+7
+function mergeSort(inputList) {
+  const { length } = inputList; 
+}
+8
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+9
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+10
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+11
+function mergeSort(inputList) {
+  const { length } = inputList; 
+}
+12
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+13
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+14
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+15
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+16
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+17
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+18
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+19
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+20
+function mergeSort(inputList) {
+  const { length } = inputLit;
+}
+21
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+22
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+23
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+24
+function mergeSort(inputList) {
+  const { length } = inputList; 
+}
+25
+function mergeSort(inputList) {
+  const { length } = inputList; 
+}
+26
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+27
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+28
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+29
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+30
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+31
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+32
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+33
+function mergeSort(inputList) {
+  const { length } = inputList; 
+}
+34
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+35
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+36
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+37
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+38
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+39
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+40
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+41
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+42
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+43
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+44
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+45
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+46
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+47
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+48
+function mergeSort(inputList) {
+  const { length } = inputList;
+}
+49
+function mergeSort(inputList) {
+}
+50
+function mergeSort(inputList){
+}
+51
+function mergeSort(inputList){
+}
+52
+function mergeSort(inputList){ 
+}
+53
+function mergeSort(inputList){ 
+}
+54
+function mergeSort(inputList){
+}
+55
+function mergeSort(inputList) {
+}
+56
+function mergeSort(inputList){
+}
+57
+function mergeSort(inputList){
+}
+58
+function mergeSort(inputList){
+}
+59
+function mergeSort(inputList){
+}
+60
+function mergeSort(inputList){
+}
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90 
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+*/
